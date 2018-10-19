ebdf908023303b38badcd0d1caf24b662db8fb03
raflitrilaksono <raflitrilaksono@gmail.com>
2018-10-19 21:51:41 +0700

    Update KCP1 (Profile) Coba di test run

--- app/src/main/java/com/example/userpc/pisangku/KCP1.java
+++ app/src/main/java/com/example/userpc/pisangku/KCP1.java
@@ -10,47 +10,11 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class KCP1 {
+public class KCP1  extends AppCompatActivity{
 
-    String photokcp, namakcp, singkatkcp, visikcp, misikcp, mottokcp;
-
-    public String getPhotokcp() { return photokcp; }
-
-    public void setPhotokcp(String photokcp) { this.photokcp = photokcp; }
-
-    public String getNamakcp() { return namakcp; }
-
-    public void setNamakcp(String namakcp) { this.namakcp = namakcp; }
-
-    public String getSingkatkcp() { return singkatkcp; }
-
-    public void setSingkatkcp(String singkatkcp) { this.singkatkcp = singkatkcp; }
-
-    public String getVisikcp() { return visikcp; }
-
-    public void setVisikcp(String visikcp) { this.visikcp = visikcp; }
-
-    public String getMisikcp() { return misikcp; }
-
-    public void setMisikcp(String misikcp) { this.misikcp = misikcp; }
-
-    public String getMottokcp() { return mottokcp; }
-
-    public void setMottokcp(String mottokcp) { this.mottokcp = mottokcp; }
-
-    public KCP1(String photokcp, String namakcp, String singkatkcp, String visikcp, String misikcp, String mottokcp) {
-
-        this.photokcp = photokcp;
-        this.namakcp = namakcp;
-        this.singkatkcp = singkatkcp;
-        this.visikcp = visikcp;
-        this.misikcp = misikcp;
-        this.mottokcp = mottokcp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kcp1);
     }
-
-    public KCP1() {
-
-    }
-
-
 }
